50d9fff6ff2d13df488aef8bac67a265bcccc28f
zalerapraxis <cjpff14@gmail.com>
2022-01-08 00:22:08 -0500

    new lines, adjusted rng rates, add activity status, add user and channel ids to config, handle 0 ordinal

--- Liam-Watcher-Bot/Services/LiamWatcherService.cs
+++ Liam-Watcher-Bot/Services/LiamWatcherService.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,23 +15,30 @@ namespace Liam_Watcher_Bot.Services
     {
         private readonly DiscordSocketClient _discord;
         private readonly Random _random;
+        private readonly IConfigurationRoot _config;
 
-        private ulong liamId = 175078909539254273; // testing mine for now, liam's id is 175078909539254273
-        private ulong liamVoiceChannel = 783345721100337184;
+        private ulong liamId = 391909581523255296; // these are safe defaults for my test server
+        private ulong liamVoiceChannel = 572584426018570243; // they should be overwritten by the config file values
         string filePath = "liamLeaveCount.txt";
         public int liamLeaveCount = 0;
 
         private bool liamLeftRecently = false;
         Timer liamLeftRecentlyTimer;
 
-        public LiamWatcherService(DiscordSocketClient discord, Random random)
+        public LiamWatcherService(DiscordSocketClient discord, IConfigurationRoot config, Random random)
         {
             _discord = discord;
+            _config = config;
             _random = random;
-            
+
+            liamId = ulong.Parse(_config["liamId"]);
+            liamVoiceChannel = ulong.Parse(_config["liamVoiceChannelId"]);
+
             LoadData();
 
             _discord.UserVoiceStateUpdated += _discord_UserVoiceStateUpdated;
+
+            _discord.SetActivityAsync(new Game("Liam. Obviously.", ActivityType.Watching));
         }
 
         private async Task SaveData()
@@ -75,7 +83,7 @@ namespace Liam_Watcher_Bot.Services
                 {
                     Console.WriteLine($"Liam left a voice channel. Starting timer...");
                     liamLeftRecently = true;
-                    liamLeftRecentlyTimer = new Timer(async delegate { liamLeftRecently = false; }, null, 300000, Timeout.Infinite);
+                    liamLeftRecentlyTimer = new Timer(async delegate { Console.WriteLine("Timed out, he's gone."); liamLeftRecently = false; }, null, 300000, Timeout.Infinite);
                 }
             }
         }
@@ -83,40 +91,52 @@ namespace Liam_Watcher_Bot.Services
         private async Task PostAboutLiamRagequit()
         {
             var channel = _discord.GetChannel(liamVoiceChannel) as ITextChannel;
-            string message = $"Liam has left voice {liamLeaveCount} times.";
 
+            string message = $"Liam has left voice {liamLeaveCount} times."; // default msg
+
+            // lists for meme posting
             List<string> lowAngerList = new List<string>
             {
+                $"Liam has left voice {liamLeaveCount} times.",
                 $"Liam has left voice {liamLeaveCount} times. Wow.",
                 $"Liam has left voice {liamLeaveCount} times. Has someone considered asking him to calm down?",
-                $"Aaaand there he goes again. That's {liamLeaveCount} times now.",
-                $"Once more doest our capricious Liam disappeareth from the voice channel and into the aether.",
-                $"New high score! Liam has wracked up {liamLeaveCount} ragequits!",
-                $"One of these days I'll think of more lines for this. Just know that he left again. For the {liamLeaveCount}{GetCorrectOrdinal(liamLeaveCount)} time."
+                $"That's {liamLeaveCount} times now. Has someone tried petting him? Yknow, like a dog. Yeah?",
+                $"Once more doest our capricious Liam disappeareth from the voice channel and into the aether...",
+                $"New high score! Liam has racked up {liamLeaveCount} ragequits!",
+                $"One of these days I'll think of more lines for this. Just know that he left again. For the {liamLeaveCount}{GetCorrectOrdinal(liamLeaveCount)} time.",
+                $"Wiam has weft voice {liamLeaveCount} times. Make suwe tuwu uwu at him now thawt he's bawck.",
+                $"Once again, Liam has left voice. This is the {liamLeaveCount}{GetCorrectOrdinal(liamLeaveCount)} time. Oh shit, he's back, stop talking.",
+                $"For the {liamLeaveCount}{GetCorrectOrdinal(liamLeaveCount)} time, Liam has ragequit from voice.",
             };
-
             List<string> highAngerList = new List<string>
             {
                 $"This motherfucker has left {liamLeaveCount} times. Jesus christ.",
                 $"Do you realize you've made me post this goddamned message {liamLeaveCount} times?",
                 $"Go get some goddamned therapy. This is how many times? Oh wait, I've been tracking. {liamLeaveCount} times. What the fuck.",
-                $"Once again, Liam has left voice. This is the {liamLeaveCount}{GetCorrectOrdinal(liamLeaveCount)} time.",
-                $"For the {liamLeaveCount}{GetCorrectOrdinal(liamLeaveCount)} time, Liam has ragequit from voice.",
                 $"Liam has left voice {liamLeaveCount} times. This isn't ragequitlogs, bro.",
+                $"Have you tried not being mad? Like, have you actually tried? ({liamLeaveCount} ragequits)",
+                $"I know you're reading this. I'm not angry, I'm just disappointed. ({liamLeaveCount} ragequits)",
+                $"There is no point pretending that you're not unmistakably fucked. Think about your life. ({liamLeaveCount} ragequits)",
+                $"He's back! Okay, bro, deep breaths... deeeeep breaths.... ({liamLeaveCount} ragequits)",
             };
 
             var chanceToPostMemes = _random.Next(0, 100);
-            if (CheckIfBetweenRange(chanceToPostMemes, 70, 95)) // moderately rare but not terribly so
+            if (CheckIfBetweenRange(chanceToPostMemes, 0, 90)) 
             {
                 var memeListPick = _random.Next(0, lowAngerList.Count);
                 message = lowAngerList[memeListPick];
             }
-            if (CheckIfBetweenRange(chanceToPostMemes, 96, 99)) // rare
+            if (CheckIfBetweenRange(chanceToPostMemes, 91, 99)) // rare
             {
                 var memeListPick = _random.Next(0, highAngerList.Count);
                 message = highAngerList[memeListPick];
             }
 
+            // overrides for dumb shit
+            if (liamLeaveCount.ToString().Contains("69") || liamLeaveCount.ToString().Contains("420"))
+                message = $"That was ragequit number {liamLeaveCount}. Nice.";
+            if (Math.Abs(liamLeaveCount) % 100 == 0) // every 100th
+                message = $"This is cause for celebration. Liam has abandoned his friends in their time of comedic need {liamLeaveCount} times.";
 
             await channel.SendMessageAsync(message);
         }
@@ -138,6 +158,7 @@ namespace Liam_Watcher_Bot.Services
                     return "nd";
                 case 3:
                     return "rd";
+                case 0:
                 case 4:
                 case 5:
                 case 6:
